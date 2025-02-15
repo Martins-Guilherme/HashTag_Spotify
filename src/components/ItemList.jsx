ef11eacad2712dea2c192f7e3e2f59e0bfad1e9b
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2025-02-15 00:27:03 -0300

    chore(ItemList): adjust item display based on route

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,19 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import SingleItem from "./SingleItem";
 
 const ItemList = ({ title, items, itemsArray, path, idPath }) => {
+  const { pathname } = useLocation();
+  const isHome = pathname === "/";
+  let finalItems;
+
+  isHome ? (finalItems = items) : (finalItems = Infinity);
+  // Se for a Home, exibe 5 itens, senão exibe todos os itens
+
   return (
     <div className="item-list">
       <div className="item-list__header">
         <h2>{title} populares</h2>
-        <Link to={path} className="item-list__link">
-          Mostrar tudo
-        </Link>
+        {isHome ? (
+          <Link to={path} className="item-list__link">
+            Mostrar tudo
+          </Link>
+        ) : (
+          <></>
+        )}
       </div>
 
       <div className="item-list__container">
         {itemsArray
-          .filter((currentValue, index) => index < items)
+          .filter((currentValue, index) => index < finalItems)
           .map((currObj, index) => {
             return (
               <SingleItem
